b7c35a7704192dfbbf7e55d593d88e649620e98f
Priyanshu Sharma <sharmapriyanshu8488@gmail.com>
2026-07-16 09:12:02 -0500

    leetcode-java || missing Elements from an array with duplicates

---
+++ src/Basicsofjava/LeetCodeProblems/MissingElement.java
@@ -0,0 +1,32 @@
+package Basicsofjava.LeetCodeProblems;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MissingElement {
+    public List<Integer> findDisappearedNumbers(int[] nums){
+        List<Integer> ans= new ArrayList<>();
+        int n=nums.length;
+        for(int index=0;index<n;index++){
+            int value =Math.abs(nums[index]);
+            int position =value-1;
+
+            if(nums[position]>0){
+                nums[position]=-nums[position];
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(nums[i]>0){
+                int valueAtThisIndex=i+1;
+                ans.add(valueAtThisIndex);
+            }
+        }
+       return ans;
+        }
+    public static  void main(String args[]){
+        int[] nums = {4, 3, 2, 7, 8, 2, 3, 1};
+        MissingElement obj = new MissingElement();
+        List<Integer> result = obj.findDisappearedNumbers(nums);
+        System.out.println(result);
+    }
+}
