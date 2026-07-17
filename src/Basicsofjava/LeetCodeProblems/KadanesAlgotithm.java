df90bc97d311366eb39eeb41be043f95d1d92b7e
Priyanshu Sharma <sharmapriyanshu8488@gmail.com>
2026-07-17 09:06:36 -0500

    Kadane's Algorithm-> Maximum Subarray

---
+++ src/Basicsofjava/LeetCodeProblems/KadanesAlgotithm.java
@@ -0,0 +1,20 @@
+package Basicsofjava.LeetCodeProblems;
+
+public class KadanesAlgotithm {
+    public static int maxSubArray(int[] nums){
+       int sum=0;
+       int maxi=Integer.MIN_VALUE;
+       for(int i=0;i<nums.length;i++){
+           sum=sum+nums[i];
+           maxi=Math.max(maxi,sum);
+           if(sum<0)
+               sum=0;
+       }
+       return maxi;
+    }
+    public static void main(String[] args) {
+        int[] nums={-2,1,-3,4,-1,2,1,-5,4};
+       int ans = maxSubArray(nums);
+        System.out.println(ans);
+    }
+}
